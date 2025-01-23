aafd021650da5650577b6b2056b5612a1fb60faf
Gokulraj03 <137242167+Gokulraj0311@users.noreply.github.com>
2025-01-23 22:41:11 +0530

    Delete age.java

--- age.java
+++
@@ -1,9 +0,0 @@
-/*Today task for coding 
-1.Check Prime Number: Write a Java program that checks whether a given number is a prime number or not. (A prime number is only divisible by 1 and itself.)
-
-2.Age Group Classification: Write a Java program that takes an age as input and prints the corresponding age group:
-
-0-12: Child
-13-19: Teenager
-20-64: Adult
-65 and above: Senior*/
