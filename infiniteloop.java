2b64007e6bbafd92738607da800b4641aa592649
Gokulraj03 <137242167+Gokulraj0311@users.noreply.github.com>
2025-01-30 18:13:22 +0530

    Create infiniteloop.java

---
+++ infiniteloop.java
@@ -0,0 +1,13 @@
+public class infiniteloop {
+    public static void main(String[] args) {
+        System.out.println("For loop Enters...!");
+       for(int i=0;i>0;i++){
+        System.out.println("This loop runs 100 times");
+       }
+       System.out.println("entry of Infinite while Loop...!");
+       int j=0;
+       while(j>0){
+        System.out.println("This while runs forever..!");
+       }
+    }
+}
