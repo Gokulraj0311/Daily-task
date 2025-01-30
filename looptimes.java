2f4aa899660738252b9635cba471e5b5249f6ff8
Gokulraj03 <137242167+Gokulraj0311@users.noreply.github.com>
2025-01-30 17:58:50 +0530

    Create looptimes.java

---
+++ looptimes.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class looptimes{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter the number of times:");
+        int times=sc.nextInt();
+        for(int i=0;i<times;i++){
+            System.out.println("ALL IS WELL");
+        }
+        
+    }
+}
