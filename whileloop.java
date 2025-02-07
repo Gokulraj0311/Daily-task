a44aa8b5a710702692ad21ddaa270ab282885195
Gokulraj03 <137242167+Gokulraj0311@users.noreply.github.com>
2025-02-07 22:45:27 +0530

    Create whileloop.java

---
+++ whileloop.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int i=10;
+        while(i>=1){
+            
+            System.out.print(n*i+" ");
+            i--;
+        }
+    }
+}
+
+/*While loop is another loop like for loop but unlike for loop it only checks for one condition.
+Here, we will use a while loop and print a number n's table in reverse order.
+
+Examples:
+
+Input: n = 1
+Output: 10 9 8 7 6 5 4 3 2 1
+Input: n = 2
+Output: 20 18 16 14 12 10 8 6 4 2
+*/
